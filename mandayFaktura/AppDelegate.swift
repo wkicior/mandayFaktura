e3cc2b63b87e5424afa98135b6a13a0158c7e63f
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-01-25 22:06:46 +0100

    mandayFaktura Cocoa project generated in XCode 9.2

---
+++ mandayFaktura/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mandayFaktura
+//
+//  Created by Wojciech Kicior on 25.01.2018.
+//  Licensed under the MIT License. See LICENSE file for more details
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mandayFaktura/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mandayFaktura
+//
+//  Created by Wojciech Kicior on 25.01.2018.
+//  Licensed under the MIT License. See LICENSE file for more details
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
