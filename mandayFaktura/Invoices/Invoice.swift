ca06ede6e6c298f2db029312433aa7091645c09f
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-01-27 18:35:35 +0100

    add invoice and invoicesrepository model classes

---
+++ mandayFaktura/Invoices/Invoice.swift
@@ -0,0 +1,17 @@
+//
+//  Invoice.swift
+//  mandayFaktura
+//
+//  Created by Wojciech Kicior on 27.01.2018.
+//  Copyright © 2018 Wojciech Kicior. All rights reserved.
+//
+
+import Foundation
+
+struct Invoice {
+    let issueDate:Date
+    let number:String
+    //let seller = Counterparty()
+    //let buyer = Counterparty()
+    
+}
---
+++ mandayFaktura/Invoices/InvoicesRepository.swift
@@ -0,0 +1,13 @@
+//
+//  InvoicesRepository.swift
+//  mandayFaktura
+//
+//  Created by Wojciech Kicior on 27.01.2018.
+//  Copyright © 2018 Wojciech Kicior. All rights reserved.
+//
+
+import Foundation
+
+class InvoicesRepository {
+    let invoices = [Invoice(issueDate: Date(), number: "01/A/2018"), Invoice(issueDate: Date(), number: "01/A/2018")]
+}
