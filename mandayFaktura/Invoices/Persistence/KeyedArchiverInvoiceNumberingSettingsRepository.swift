c6850273a0ab0a836b16848121b31ef8f1e92513
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-03-25 20:45:44 +0200

    create KeyedArchiverInvoiceNumberingSettingsRepository

--- mandayFaktura/Invoices/Persistence/KeyedArchiverInvoiceNumberingSettingsRepository.swift
+++ mandayFaktura/Invoices/Persistence/KeyedArchiverInvoiceNumberingSettingsRepository.swift
@@ -8,14 +8,72 @@
 
 import Foundation
 
-class InMemoryInvoiceNumberingSettingsRepository: InvoiceNumberingSettingsRepository {
-    var invoiceNumberingSettings = InvoiceNumberingSettings(separator: "/", segments: [NumberingSegment(type: .incrementingNumber, value: nil), NumberingSegment(type: .fixedPart, value: "A"), NumberingSegment(type: .year, value: nil)])
+@objc(NumberingSegmentCoding) private class NumberingSegmentCoding: NSObject, NSCoding {
+    let numberingSegment: NumberingSegment
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.numberingSegment.type.rawValue, forKey: "type")
+        coder.encode(self.numberingSegment.fixedValue, forKey: "fixedValue")
+    }
+    
+    required convenience init?(coder decoder: NSCoder) {
+        guard let fixedValue = decoder.decodeObject(forKey: "fixedValue") as? String?
+            else { return nil }
+        let type = NumberingSegmentType(rawValue: (decoder.decodeObject(forKey: "type") as? String)!)!
+        self.init(NumberingSegment(type: type, value: fixedValue))
+    }
+    
+    init(_ numberingSegment: NumberingSegment) {
+        self.numberingSegment = numberingSegment
+    }
+    
+}
+
+@objc(InvoiceNumberingSettingsCoding) private class InvoiceNumberingSettingsCoding: NSObject, NSCoding {
+    let invoiceNumberingSettings: InvoiceNumberingSettings
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.invoiceNumberingSettings.separator, forKey: "separator")
+        coder.encode(self.invoiceNumberingSettings.segments.map{s in NumberingSegmentCoding(s)}, forKey: "segments")
+    }
+    
+    required convenience init?(coder decoder: NSCoder) {
+        guard let separator = decoder.decodeObject(forKey: "separator") as? String,
+            let segmentsCoding = decoder.decodeObject(forKey: "segments") as? [NumberingSegmentCoding]
+            else { return nil }
+        let segments = segmentsCoding.map({c in c.numberingSegment})
+        
+        self.init(InvoiceNumberingSettings(separator: separator, segments: segments))
+    }
+    
+    init(_ invoiceNumberingSettings: InvoiceNumberingSettings) {
+        self.invoiceNumberingSettings = invoiceNumberingSettings
+    }
+}
+
+
+class KeyedArchiverInvoiceNumberingSettingsRepository: InvoiceNumberingSettingsRepository {
+    private let key = "invoiceNumberingSettings" + AppDelegate.keyedArchiverProfile
+    
     func getInvoiceNumberingSettings() -> InvoiceNumberingSettings? {
-        return self.invoiceNumberingSettings
+        return self.invoiceNumberingSettingsCoding.invoiceNumberingSettings
     }
     
     func save(invoiceNumberingSettings: InvoiceNumberingSettings) {
-        self.invoiceNumberingSettings = invoiceNumberingSettings
+        self.invoiceNumberingSettingsCoding = InvoiceNumberingSettingsCoding(invoiceNumberingSettings)
+    }
+    
+    private var invoiceNumberingSettingsCoding: InvoiceNumberingSettingsCoding {
+        get {
+            if let data = UserDefaults.standard.object(forKey: key) as? NSData {
+                return  NSKeyedUnarchiver.unarchiveObject(with: data as Data) as! InvoiceNumberingSettingsCoding
+            }
+            return InvoiceNumberingSettingsCoding(InvoiceNumberingSettings(separator: "/", segments: [NumberingSegment(type: .incrementingNumber, value: nil), NumberingSegment(type: .year, value: nil)]))
+        }
+        set {
+            let data = NSKeyedArchiver.archivedData(withRootObject: newValue)
+            UserDefaults.standard.set(data, forKey: key)
+        }
     }
 }
 
